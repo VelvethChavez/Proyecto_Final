daf36621894cb6acf42bdfe8b822eecf485b28a8
huskymasmas <rcuettolusky@gmail.com>
2024-05-10 18:11:52 -0600

    Revert " tablas"
    This reverts commit 9f762c4cfb506e420e70f2c85a0f4128b64f8e93.

--- src/main/java/com/mycompany/proyectofinal/Estado.java
+++ src/main/java/com/mycompany/proyectofinal/Estado.java
@@ -36,16 +36,9 @@ public class Estado extends javax.swing.JFrame {
             Class[] types = new Class [] {
                 java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
             };
-            boolean[] canEdit = new boolean [] {
-                false, false, false, false, false
-            };
 
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
-            }
-
-            public boolean isCellEditable(int rowIndex, int columnIndex) {
-                return canEdit [columnIndex];
             }
         });
         jScrollPane1.setViewportView(tblEstadoPedidos);
